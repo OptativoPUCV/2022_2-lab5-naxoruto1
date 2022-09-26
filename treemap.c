1759792991877284ea21fa6b52bd337195a4f9be
Replit user <>
2022-09-26 01:37:17 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -48,29 +48,8 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)){
 }
 
 
-Pair * searchTreeMap(TreeMap * tree, void* key){}
-  
-  if(tree == NULL){ // si es nulo
-    return NULL;
-  }
+void insertTreeMap(TreeMap * tree, void* key, void * value) {
 
-  TreeNode *aux = tree->root;
-
-  
-  while(aux != NULL){
-    tree->current = aux;
-    if(tree->lower_than(aux->pair->key, key) == 1){
-      aux = aux->right;
-    }else if(tree->lower_than(key, aux->pair->key) == 1){
-      aux = aux->left;
-      }else break;
-  }
-  
-   if(is_equal(tree,tree->current->pair->key,key)){
-    return tree->current->pair;
-  }
-  
-  return NULL;
 }
 
 TreeNode * minimum(TreeNode * x){
@@ -95,8 +74,29 @@ void eraseTreeMap(TreeMap * tree, void* key){
 
 
 
-Pair * searchTreeMap(TreeMap * tree, void* key) {
+Pair * searchTreeMap(TreeMap * tree, void* key){}
+  
+  if(tree == NULL){ // si es nulo
     return NULL;
+  }
+
+  TreeNode *aux = tree->root;
+
+  
+  while(aux != NULL){
+    tree->current = aux;
+    if(tree->lower_than(aux->pair->key, key) == 1){
+      aux = aux->right;
+    }else if(tree->lower_than(key, aux->pair->key) == 1){
+      aux = aux->left;
+      }else break;
+  }
+  
+   if(is_equal(tree,tree->current->pair->key,key)){
+    return tree->current->pair;
+  }
+  
+  return NULL;
 }
 
 
